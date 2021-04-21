440b58ff14c3698c20d59d196569aca6e94eae60
Pedro Passos <73182976+pedro-22002182@users.noreply.github.com>
2021-04-21 19:43:06 +0100

    Atualizao ataque inimigo + bullet

---
+++ DJD_StarWars/Assets/scripts/DestroyOnCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnCollision : MonoBehaviour
+{
+    [SerializeField]
+    private string tag;
+
+    [SerializeField]
+    private GameObject effect;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ((tag == collision.tag) || (tag == ""))
+        {
+            Destroy(gameObject);
+
+            if (effect)
+            {
+                Instantiate(effect, transform.position, transform.rotation);
+            }
+        }
+    }
+}
---
+++ DJD_StarWars/Assets/scripts/MoveForward.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    private Vector3 moveVector;
+
+    [SerializeField]
+    private float speed = 400;
+
+    float   timer = 0.0f;
+    Vector3 startPos;
+
+    void Start()
+    {
+        //use object diretion   
+        moveVector = transform.up;
+        moveVector.Normalize();
+
+        startPos = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        timer += Time.fixedDeltaTime;
+
+        transform.position = startPos + moveVector * speed * timer;
+    }
+
+    void Update()
+    {
+        
+    }
+}
