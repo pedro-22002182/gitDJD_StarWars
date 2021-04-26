0f4f062a58e91a4599f99aa00c52e888913735d3
Pedro Passos <73182976+pedro-22002182@users.noreply.github.com>
2021-04-26 20:02:01 +0100

    Sistema Dialogo
    Para tutorial e conversas.
    + correçoes nas dimensoes

---
+++ DJD_StarWars/Assets/scripts/SistemaConversa/DialogueManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject dBox;
+
+    [SerializeField]
+    private Text dText;
+
+    [SerializeField]
+    private Image dImage;
+
+    private List<string> dialog;
+    private bool dialogActive;
+    private int index, indexMax;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogActive = false;
+        index = 0;
+
+        dBox.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(dialogActive && Input.GetMouseButtonDown(0)){
+
+            index += 1;
+
+            if(index < indexMax)
+            {
+                dText.text = dialog[index];
+            }
+            else
+            {
+                dBox.SetActive(false); 
+                index = 0;
+                dialogActive = false;
+            }
+        }
+    }
+
+    public void ShowBox(List<string> dialogue){
+
+        dBox.SetActive(true);
+        dialogActive = true;
+        
+        dText.text = dialogue[index];
+
+        indexMax = dialogue.Count;
+        dialog = dialogue;
+    }
+}
